6f05f99d4e8d956ae2882f3ad3ef2c2dbd25d7ec
Jeff Chen <hello@jeff.yt>
2018-12-29 21:39:17 -0600

    baby's first apollo

--- src/frontend/tsx/App.tsx
+++ src/frontend/tsx/App.tsx
@@ -2,25 +2,42 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../scss/App.css';
 
+import ApolloClient from "apollo-boost";
+import { Query, ApolloProvider } from "react-apollo";
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql"
+});
+
+const User = () => (
+  <Query
+    query={gql`
+      {
+        user(id: "lol") {
+          id
+          email
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <div><p>{`${data.user.id}: ${data.user.email}`}</p></div>;
+    }}
+  </Query>
+);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <User />
+        </div>
+      </ApolloProvider>
     );
   }
 }
